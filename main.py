c4df59639315731bbde3d983752d3812633051b5
hgpnguyen <phucgia1808@gmail.com>
2024-11-15 14:40:27 +0700

    Add thread

--- main.py
+++ main.py
@@ -1,8 +1,40 @@
 import requests
 import os
-import geo
+from geopy.geocoders import Nominatim
 from dotenv import load_dotenv
+from threadParallel import parallel
+
 load_dotenv()
+
+def get_all_cities(latMin: float, latMax: float, lonMin: float, lonMax: float):
+    geolocator = Nominatim(user_agent="abcd")
+    cities = set()
+    results = []
+    lat, lon = latMin, lonMin
+    STEP = 0.5
+    count = 0
+    while lat <= latMax:
+        while lon <= lonMax:
+            count += 1
+            coord = f"{lat},{lon}"
+            location = geolocator.reverse(coord, exactly_one=True)
+            if not location:
+                lon += STEP
+                continue
+            address = location.raw['address']
+            city = address.get('city', '')
+            if city and city not in cities:
+                cities.add(city)
+                results.append((city, lat, lon))
+            lon += STEP
+        lon = lonMin
+        lat += STEP
+    print(results)
+    print(len(results))
+    print(count)
+    return results
+
+
 
 def get_weather(lon: float, lat: float):
     api_key = os.environ.get('OPENWEATHER_API', '')
@@ -16,8 +48,9 @@ def printWeather(weatherJson):
     print(weatherMssg) 
 
 def main():
-    weather, status = get_weather(44.34, 10.99)
-    printWeather(weather)
+    #weather, status = get_weather(44.34, 10.99)
+    #printWeather(weather)
+    parallel(200, 44.34, 54.34, 10.99, 20.99)
 
 if __name__=="__main__":
     main()
